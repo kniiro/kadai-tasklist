3385a13191d5c09c196eb2dcec6bda97fadaa24c
kenichironiiro <niiro1231@gmail.com>
2017-08-02 06:48:15 +0000

    add status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801051307) do
+ActiveRecord::Schema.define(version: 20170801060518) do
 
   create_table "tasklists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170801051307) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
+    t.string   "status"
   end
 
 end
